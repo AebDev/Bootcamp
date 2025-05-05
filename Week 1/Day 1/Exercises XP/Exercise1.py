118cdf59d5b7681e2442729f2c3a4bdc749747bf
unknown <mohcine.aeb@gmail.com>
2025-05-05 16:08:15 +0100

    day1_exercies

---
+++ Week 1/Day 1/Exercises XP/Exercise1.py
@@ -0,0 +1,51 @@
+first = "Hello World"
+
+#This is a comment.
+
+print("I AM A COMPUTER!")
+
+if 1<2 and 4>2:
+    print("Math is fun.")
+
+nope = None
+
+
+value = "true" and "false"
+
+len("What's my length?")
+
+("i am shouting").upper()
+
+int("1000")
+
+str(4) + "real"
+
+rec1 = 3 * "cool"
+
+#rec2 = 1 / 0
+
+type([])
+
+name = input("insert your name : ")
+
+number = int(input("insert a number : "))
+if number < 0 :
+    print("That number is less than 0!")
+elif number > 0 :
+    print("That number is greater than 0!")
+else :
+    print("You picked 0!")
+
+
+"l" in "apple"
+
+"y" in "xylophone"
+
+"my_string".islower()
+
+
+
+
+
+
+
