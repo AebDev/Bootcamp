9e1b811536e71fead8194587ada25b31da84a3c8
unknown <mohcine.aeb@gmail.com>
2025-05-13 12:20:02 +0100

    W2D2 Exercices Xp

---
+++ Week_2/Day_2/Exercises/Exercises_XP/Exercise_2.sql
@@ -0,0 +1,15 @@
+select * from customer;
+select first_name || ' ' || last_name as full_name from customer;
+select distinct create_date from customer;
+select * from customer order by first_name desc;
+select film_id,title,description,release_year,rental_rate from film order by rental_rate asc;
+select address,phone from address inner join customer on customer.address_id = address.address_id where district = 'Texas';
+select * from film where film_id = 15 or film_id = 150;
+select film_id,title,description,film.length,rental_rate from film where title like 'Mad Max';
+select film_id,title,description,film.length,rental_rate from film where title like 'Ma%';
+select * from film order by replacement_cost asc limit 10;
+select * from film order by replacement_cost asc offset 10 limit 10;
+select first_name,last_name,amount,payment_date from customer inner join payment on customer.customer_id=payment.customer_id order by customer.customer_id asc;
+select * from film where film.film_id not in (select film_id from inventory);
+select city,country from city inner join country on city.country_id = country.country_id;
+select customer.customer_id,first_name,last_name,amount,payment_date from customer inner join payment on customer.customer_id=payment.customer_id order by staff_id;
