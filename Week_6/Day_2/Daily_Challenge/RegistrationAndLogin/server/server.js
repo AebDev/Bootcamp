ea5d88ab9ac201b39434339dd2401c38e9680b39
unknown <mohcine.aeb@gmail.com>
2025-06-11 00:53:26 +0100

    W6D2 Daily Challenge

--- Week_6/Day_2/Daily_Challenge/RegistrationAndLogin/server/server.js
+++ Week_6/Day_2/Daily_Challenge/RegistrationAndLogin/server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const userRoutes = require('./routes/userRoutes');
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+app.use(express.json());
+
+app.use('/users', userRoutes);
+
+
+app.listen(3000, () => console.log(`Server running on port 3000`));
